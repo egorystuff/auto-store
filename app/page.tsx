bc750f3dbc0b78ca5cb9b46a46078d82712dce0f
Yahor Yankavets <mr.yankovec@gmail.com>
2024-11-13 22:24:59 +0300

    bugs fixed

--- app/page.tsx
+++ app/page.tsx
@@ -3,34 +3,30 @@
 import React from "react";
 import Autoplay from "embla-carousel-autoplay";
 import { Categories, Container, Title } from "@/components/shared";
-import { Card, CardContent, Carousel, CarouselContent, CarouselItem } from "@/components/ui";
+import { Carousel, CarouselContent, CarouselItem } from "@/components/ui";
+import Image from "next/image";
 
 export default function Home() {
   const plugin = React.useRef(Autoplay({ delay: 2000, stopOnInteraction: true }));
 
   return (
     <>
-      <Container className='flex items-center justify-center mt-10'>
-        <Carousel
-          plugins={[plugin.current]}
-          className='w-full max-w-xs'
-          onMouseEnter={plugin.current.stop}
-          onMouseLeave={plugin.current.reset}>
-          <CarouselContent>
-            {Array.from({ length: 5 }).map((_, index) => (
-              <CarouselItem key={index}>
-                <div className='p-1'>
-                  <Card className='border-none'>
-                    <CardContent className='flex aspect-square items-center justify-center p-6'>
-                      <span className='text-4xl font-semibold'>{index + 1}</span>
-                    </CardContent>
-                  </Card>
-                </div>
-              </CarouselItem>
-            ))}
-          </CarouselContent>
-        </Carousel>
-      </Container>
+      <div className='border border-b'>
+        <Container className='flex items-center justify-center mt-10 mb-10'>
+          <Carousel plugins={[plugin.current]} onMouseEnter={plugin.current.stop} onMouseLeave={plugin.current.reset}>
+            <CarouselContent>
+              {Array.from({ length: 4 }).map((_, index) => (
+                <CarouselItem key={index}>
+                  <div>
+                    {/* <img src={`/cars/${index + 1}.jpg`} alt='car' className='w-full flex items-center justify-between' /> */}
+                    <Image src={`/cars/${index + 1}.jpg`} alt='car' width={1280} height={720} />
+                  </div>
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+          </Carousel>
+        </Container>
+      </div>
 
       <Container className='mt-10'>
         <Title text='Купить авто из Америки, Европы и Азии' size='lg' className='font-bold' />
