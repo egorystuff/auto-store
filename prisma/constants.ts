5175b5af46a4874a5ee4d2a13b11e7f108fb99e9
Yahor Yankavets <mr.yankovec@gmail.com>
2024-11-30 23:53:45 +0300

    update for DB

---
+++ prisma/constants.ts
@@ -0,0 +1,91 @@
+export const categories = [
+  {
+    name: "Авто из Америки",
+  },
+  {
+    name: "Авто из Европы",
+  },
+  {
+    name: "Авто из Китая",
+  },
+];
+
+export const products = [
+  {
+    name: "BMW 5-Series G30",
+    yearOfManufacture: 2021,
+    price: 26000,
+    imageUrl: "https://intercarex.by/auto-germany/bmw-5-europ-img.jpg",
+    categoryId: 1,
+  },
+  {
+    name: "Volvo XC60",
+    yearOfManufacture: 2022,
+    price: 18000,
+    imageUrl: "https://intercarex.by/auto-germany/volvo-xc-60-europe-img.jpg",
+    categoryId: 1,
+  },
+  {
+    name: "Audi Q8",
+    yearOfManufacture: 2022,
+    price: 45000,
+    imageUrl: "https://intercarex.by/auto-germany/audi-q8-europ-img.jpg",
+    categoryId: 1,
+  },
+  {
+    name: "Mercedes-benz GLE",
+    yearOfManufacture: 2022,
+    price: 42000,
+    imageUrl: "https://intercarex.by/auto-germany/mercedes-gle-europ-img.jpg",
+    categoryId: 2,
+  },
+  {
+    name: "Opel Insignia Combi",
+    yearOfManufacture: 2021,
+    price: 9000,
+    imageUrl: "https://intercarex.by/auto-germany/opel-insignia-europ-img.jpg",
+    categoryId: 2,
+  },
+  {
+    name: "Volkswagen Tiguan",
+    yearOfManufacture: 2021,
+    price: 14500,
+    imageUrl: "https://intercarex.by/auto-germany/vw-tiguan-img.jpg",
+    categoryId: 2,
+  },
+  {
+    name: "Peugeot 5008",
+    yearOfManufacture: 2021,
+    price: 13000,
+    imageUrl: "https://intercarex.by/auto-germany/peugeot-5008-img.jpg",
+    categoryId: 2,
+  },
+  {
+    name: "BYD Yuan UP",
+    yearOfManufacture: 2024,
+    price: 21900,
+    imageUrl: "https://zubr-electro.by/wp-content/uploads/2024/11/IMG_7055.jpg",
+    categoryId: 3,
+  },
+  {
+    name: "GEELY Geome Star Wish",
+    yearOfManufacture: 2024,
+    price: 17500,
+    imageUrl: "https://zubr-electro.by/wp-content/uploads/2024/11/bdaed53555ef4fa395db0ae536e2fa911200x0-2.webp",
+    categoryId: 3,
+  },
+  {
+    name: "XIAOMI SU7",
+    yearOfManufacture: 2024,
+    price: 31700,
+    imageUrl: "https://zubr-electro.by/wp-content/uploads/2024/03/1-2.jpg.webp",
+    categoryId: 3,
+  },
+  {
+    name: "ZEEKR 007",
+    yearOfManufacture: 2024,
+    price: 33000,
+    imageUrl: "https://zubr-electro.by/wp-content/uploads/2024/01/Main-scaled.jpg.webp",
+    categoryId: 3,
+  },
+];
