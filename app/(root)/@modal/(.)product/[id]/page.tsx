50ea6c4831343724632914d15753b89c3b817393
Yahor Yankavets <mr.yankovec@gmail.com>
2025-07-19 21:49:46 +0300

    Update page.tsx

--- app/(root)/@modal/(.)product/[id]/page.tsx
+++ app/(root)/@modal/(.)product/[id]/page.tsx
@@ -2,7 +2,8 @@ import { prisma } from "@/prisma/prisma-client";
 import { ChooseProductModal } from "@/shared/components/shared";
 import { notFound } from "next/navigation";
 
-export default async function ProductModalPage({ params: { id } }: { params: { id: string } }) {
+export default async function ProductModalPage({ params }: { params: { id: string } }) {
+  const { id } = await params;
   const product = await prisma.product.findFirst({
     where: {
       id: Number(id),
